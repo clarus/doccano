c10712e00f80d0a802ae751ce8caf7a974713931
Guillaume Claret <dev@clarus.me>
2019-05-25 19:03:38 +0200

    WIP

--- app/server/static/js/sequence_labeling_label_selector.vue
+++ app/server/static/js/sequence_labeling_label_selector.vue
@@ -3,13 +3,14 @@ header.card-header
   div.card-header-title.has-background-royalblue
     div
       div.field
-        input.input(
-          placeholder="Search label"
-          type="text"
-          v-model="searchLabel"
-        )
-      div.field.is-grouped.is-grouped-multiline(v-if="searchLabel")
-        div.control(v-for="label in filteredLabels")
+        div.control
+          input.input(
+            placeholder="Search label"
+            type="text"
+            v-model="searchLabel"
+          )
+      div.field.is-grouped.is-grouped-multiline
+        div.control(v-for="label in filteredLabels.labels")
           div.tags.has-addons
             a.tag.is-medium(
               v-bind:style="{ \
@@ -18,6 +19,8 @@ header.card-header
               }"
               v-on:click="annotate(label.id)"
             ) {{ label.text }}
+        p.has-text-white(v-if="filteredLabels.hasMore") ...
+
 </template>
 
 <style scoped>
@@ -43,8 +46,16 @@ export default {
   }),
   computed: {
     filteredLabels() {
-      return this.labels.filter(label => label.text.toLowerCase().includes(this.searchLabel.toLowerCase()));
-    }
+      const maxNbOfResults = 25;
+      const labels = this.labels.filter(
+        label => label.text.toLowerCase().includes(this.searchLabel.toLowerCase()),
+      );
+
+      return {
+        hasMore: labels.length > maxNbOfResults,
+        labels: labels.slice(0, maxNbOfResults),
+      };
+    },
   },
 };
 </script>
